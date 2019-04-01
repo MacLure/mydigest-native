64aed8f09b4978f8d0e8502cad679486a0ab5766
Malcolm <malcolm.maclure@gmail.com>
2019-04-01 13:22:37 -0400

    exchange rates

--- Currency.js
+++ Currency.js
@@ -10,6 +10,7 @@ import {
 } from "react-native";
 import { formatTestResults } from "@jest/test-result";
 import { Spinner } from "native-base";
+import { getCurrencyRates } from "./service";
 
 type Props = {};
 export default class Currency extends Component<Props> {
@@ -17,54 +18,37 @@ export default class Currency extends Component<Props> {
     super(props);
 
     this.state = {
-      data: null,
-      loaded: true,
-      error: null
+      currencyRates: {}
     };
   }
-  baseURL = "https://jsonplaceholder.typicode.com";
 
-  getData = e => {
-    this.setState({ loaded: false, error: null });
-    let url = this.baseURL + "/comments";
-    let h = new Headers();
-    h.append("Authorization", "Bearer asdfasdf");
-    h.append("X-Client", "malcolm");
-    let req = new Request(url, {
-      headers: h,
-      method: "GET"
-    });
-    fetch(req)
-      .then(response => response.json())
-      .then(this.showData)
-      .catch(this.errorMessage);
+  componentDidMount = () => {
+    this.getExchangeRates();
   };
 
-  showData = data => {
-    this.setState({ loaded: true, data });
-    console.log(data);
+  getExchangeRates = () => {
+    getCurrencyRates()
+      .then(currencyRates => {
+        this.setState({ currencyRates, refreshing: false });
+      })
+      .catch(() => this.setState({ refreshing: false }));
   };
-
-  errorMessage = e => {
-    this.setState({ loaded: true, error: e.message });
-  };
-
-  componentDidMount() {
-    // this.getData()
-  }
 
   render() {
+    const CAD2USD =
+      this.state.currencyRates["USDCAD"] / this.state.currencyRates["USDUSD"];
+    const CAD2CAD =
+      this.state.currencyRates["USDCAD"] / this.state.currencyRates["USDCAD"];
+    const CAD2JPY =
+      this.state.currencyRates["USDCAD"] / this.state.currencyRates["USDJPY"];
+    const CAD2EUR =
+      this.state.currencyRates["USDCAD"] / this.state.currencyRates["USDEUR"];
+
     return (
       <ScrollView style={styles.container}>
-        {!this.state.loaded && <Spinner color="cornflowerblue" />}
-        <Text style={{ fontSize: 24 }}>API Calls</Text>
-        <Button title="Get Data" onPress={this.getData} />
-        {this.state.error != null && <Text>{this.state.error}</Text>}
-        {this.state.data &&
-          this.state.data.length > 0 &&
-          this.state.data.map(comment => (
-            <Text key={comment.id}>{comment.email}</Text>
-          ))}
+        <Text>$: {CAD2USD.toFixed(2)}</Text>
+        <Text>¥: {CAD2JPY.toFixed(2)}</Text>
+        <Text>€: {CAD2EUR.toFixed(2)}</Text>
       </ScrollView>
     );
   }
